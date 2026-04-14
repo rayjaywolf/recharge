4ba25967e69632ed9bf58a3cb60ad18893ea159e
rayjay8 <rayjay9@proton.me>
2026-04-14 13:19:53 +0530

    f' up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,33 @@
-import { Geist, Geist_Mono, Merriweather, Instrument_Sans, Lora, JetBrains_Mono, Inter } from "next/font/google"
+import {
+  Geist,
+  Geist_Mono,
+  Merriweather,
+  Instrument_Sans,
+  Lora,
+  JetBrains_Mono,
+  Inter,
+} from "next/font/google"
+import type { Metadata } from "next"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/sonner"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-const instrumentSansHeading = Instrument_Sans({subsets:['latin'],variable:'--font-heading'});
+const instrumentSansHeading = Instrument_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
 
-const lora = Lora({subsets:['latin'],variable:'--font-serif'});
+const lora = Lora({ subsets: ["latin"], variable: "--font-serif" })
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
 
 export const metadata: Metadata = {
   title: "Recharge",
@@ -27,14 +43,20 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
       className={cn(
-                          "antialiased"
-                        , lora.variable, instrumentSansHeading.variable, jetbrainsMono.variable, "font-sans", inter.variable)}
+        "antialiased",
+        lora.variable,
+        instrumentSansHeading.variable,
+        jetbrainsMono.variable,
+        "font-sans",
+        inter.variable
+      )}
     >
       <body>
         <ThemeProvider>
           {children}
           <Toaster />
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   )
